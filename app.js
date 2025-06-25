de80062b798c4a5ede6feb3fb5d2bb05ab7a40f0
jalmontegit <jalmonte721@gmail.com>
2025-06-25 18:46:21 -0400

    Lab completed, cleaned up the coding and refined the calculator a bit more, might change things up on CSS

--- app.js
+++ app.js
@@ -1,103 +1,87 @@
 /*-------------------------------- Constants --------------------------------*/
-const buttons = document.querySelectorAll('.button');
-const calculator = document.querySelector('#calculator');
-const displayElement = document.querySelectorAll('.display');
-// const additionOperator = ;
+
+
 
 /*-------------------------------- Variables --------------------------------*/
 
+// make calculator always show "0" on the display
+
 /*------------------------ Cached Element References ------------------------*/
 
+const buttons = document.querySelectorAll('.button');
+const calculator = document.querySelector('#calculator');
+const displayElement = document.querySelectorAll('.display');
+const displayAlways = document.querySelector('.display').innerText = '0';
+
 /*----------------------------- Event Listeners -----------------------------*/
-// buttons.forEach((button) => {
-//   button.addEventListener('click', (event) => {
-//     // This log is for testing purposes to verify we're getting the correct value
-//     console.log(event.target.innerText);
-//     // Future logic to capture the button's value would go here...
-//   });
-// });
 
 calculator.addEventListener('click', (event) => {
-  // This log is for testing purposes to verify we're getting the correct value
-  // You have to click a button to see this log
-  console.log(event.target.innerText);
 
+  // console.log(event.target.innerText);
 
-  // Example
   if (event.target.classList.contains('number')) {
     const numberElement = event.target.innerText;
-    // Do something with a number
-    console.log('this is a number');
-    // display number on the display
+    // console.log('this is a number');
+   
     displayElement.forEach(display => {
+      if (display.innerText === '0') {
+        display.innerText = '';
+      }
       display.innerText += numberElement;
     });
+  };
 
-
-};
-
-
-  // Example
   if (event.target.classList.contains('operator')) {
     const operatorElement = event.target.innerText;
-    // Do something with an operator
-    console.log('this is an operator');
+    // console.log('this is an operator');
     displayElement.forEach(display => {
     display.innerText += operatorElement;
         });
-
-  }
-
-  // Example
+  };
 
     if (event.target.innerText === 'C') {
-    // Do something with this operator
-    console.log('clear!');
+    // console.log('clear!');
     displayElement.forEach(display => {
-    display.innerText = '';
+    display.innerText = '0';
     });
-  }
+  };
+
     if (event.target.innerText === '=') {
-    // Do something with this operator
-    console.log('total!');
+    // console.log('total!');
     calculateTotal();
-  }
+  };
+
 });
 
 
 /*-------------------------------- Functions --------------------------------*/
 
-//create a function which calculates the total based on the display value
 function calculateTotal() {
-  // Get the current display value
-  const displayValue = displayElement[0].innerText;
-  
-  // Split the display value into numbers and operators
-  const numbers = displayValue.split(/[\+\-\*\/]/).map(Number);
-  const operator = displayValue.split(/[0-9]+/).filter(op => op !== '');
 
-  // Initialize total with the first number
+  const displayValue = displayElement[0].innerText;
+  const numbers = displayValue.split(/[\+\-\*\/]/).map(Number);
+  const operator = displayValue.split(/[0-9]+/).filter(oprt => oprt !== '');
+
   let total = 0;
   let number1 = numbers[0];
   let number2 = numbers[1];
-
-if (operator == '+') {
+  
+  if (operator == '+') {
     total = number1 + number2;
-}
-else if (operator == '-') {
+  }
+  else if (operator == '-') {
     total = number1 - number2;
-}
-else if (operator == '*') {
+  }
+  else if (operator == '*') {
     total = number1 * number2;
-}
-else if (operator == '/') {
+  }
+  else if (operator == '/') {
     total = number1 / number2;
-}
-else {
+  }
+  else {
     total = number1;
-};
+  };
 
-  // Display the total in the first display element
   displayElement[0].innerText = total;
 };
 
